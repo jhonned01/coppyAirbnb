e810f87fc81a196787ab6c8aa1f07d6d07319f21
jhonned01 <jhonned01@gmail.com>
2021-08-19 14:38:44 -0500

    firs commit

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 ">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUNT</h5>
+        <p>How Air Work</p>
+        <p>Newsoom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is no a real site</p>
+        <p>its a pretty awesome clone</p>
+        <p>Refereas accepted</p>
+        <p>jhonned01</p>
+      </div>
+      <div lassName="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORTS</h5>
+        <p>Accessibility</p>
+        <p>This is no a real site</p>
+        <p>its a pretty awesome clone</p>
+        <p>Refereas accepted</p>
+        <p>jhonned01</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Accessibility</p>
+        <p>This is no a real site</p>
+        <p>its a pretty awesome clone</p>
+        <p>Refereas accepted</p>
+        <p>jhonned01</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
